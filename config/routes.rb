fc8123e510f58ed8208c0db648445da540114d33
iwabuchi4 <mgrmgm315@gmail.com>
2021-09-01 09:30:43 +0000

    login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   get 'home' => 'static_pages#home'
   get 'help' => 'static_pages#help'
@@ -7,5 +6,8 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get  'signup' => "users#new"
+  get 'login' => "sessions#new"
+  post 'login' => "sessions#create"
+  delete 'logout' => "sessions#destroy"
   resources :users
 end
