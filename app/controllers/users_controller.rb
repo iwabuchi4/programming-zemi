e949edf236201dcc63a017eebfebe5fc0ee699cd
iwabuchi4 <mgrmgm315@gmail.com>
2021-09-23 16:17:38 +0000

    login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -10,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)  
     if @user.save
+      log_in @user
       redirect_to @user
      # 保存の成功をここで扱う。
     else
@@ -17,9 +20,29 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end  
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end    
+
   private
 	
 	  def user_params
 		  params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	  end
+
+    def logged_in_user
+      unless logged_in?
+        redirect_to login_url
+      end
+    end
+
 end
